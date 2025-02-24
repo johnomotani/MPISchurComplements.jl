3380b73983662fe7ef42ae7bbd53c623eef359c9
John Omotani <john.omotani@ukaea.uk>
2025-02-24 22:58:39 +0000

    Allow a shared-memory-MPI parallelized A_factorization

--- src/MPISchurComplements.jl
+++ src/MPISchurComplements.jl
@@ -24,8 +24,9 @@ export MPISchurComplement, mpi_schur_complement, update_schur_complement!, ldiv!
 
 using LinearAlgebra
 import LinearAlgebra: ldiv!
+using MPI
 
-mutable struct MPISchurComplement{TA,TB,TC,TSC,TSCF,TAiu,Ttv,Tbv}
+mutable struct MPISchurComplement{TA,TB,TC,TSC,TSCF,TAiu,Ttv,Tbv,Tsc<:Union{MPI.Comm,Nothing}}
     A_factorization::TA
     Ainv_dot_B::TB
     C::TC
@@ -34,6 +35,9 @@ mutable struct MPISchurComplement{TA,TB,TC,TSC,TSCF,TAiu,Ttv,Tbv}
     Ainv_dot_u::TAiu
     top_vec_buffer::Ttv
     bottom_vec_buffer::Tbv
+    shared_comm::Tsc
+    shared_comm_rank::Int
+    shared_comm_size::Int
 end
 
 """
@@ -65,7 +69,7 @@ function mpi_schur_complement(A_factorization, B::AbstractMatrix, C::AbstractMat
                               D::AbstractMatrix, Ainv_dot_B::AbstractMatrix,
                               schur_complement::AbstractMatrix,
                               Ainv_dot_u::AbstractVector, top_vec_buffer::AbstractVector,
-                              bottom_vec_buffer::AbstractVector)
+                              bottom_vec_buffer::AbstractVector; shared_MPI_comm=nothing)
     @boundscheck size(A_factorization, 1) == size(B, 1) || error(BoundsError, " Rows in A_factorization do not match rows in B")
     @boundscheck size(A_factorization, 2) == size(C, 2) || error(BoundsError, " Columns in A_factorization do not match columns in C")
     @boundscheck size(D, 1) == size(C, 1) || error(BoundsError, " Rows in C do not match rows in C")
@@ -75,14 +79,30 @@ function mpi_schur_complement(A_factorization, B::AbstractMatrix, C::AbstractMat
     @boundscheck size(A_factorization, 1) == size(top_vec_buffer, 1) || error(BoundsError, " Rows in A_factorization do not match rows in top_vec_buffer")
     @boundscheck size(D, 1) == size(bottom_vec_buffer, 1) || error(BoundsError, " Rows in D do not match rows in bottom_vec_buffer")
 
+    if shared_MPI_comm === nothing
+        shared_comm_rank = 0
+        shared_comm_size = 1
+    else
+        shared_comm_rank = MPI.Comm_rank(shared_MPI_comm)
+        shared_comm_size = MPI.Comm_size(shared_MPI_comm)
+    end
+
     ldiv!(Ainv_dot_B, A_factorization, B)
-    mul!(schur_complement, C, Ainv_dot_B)
-    @. schur_complement = D - schur_complement
-    schur_complement_factorization = lu(schur_complement)
+
+    if shared_comm_rank == 0
+        mul!(schur_complement, C, Ainv_dot_B)
+        @. schur_complement = D - schur_complement
+        schur_complement_factorization = lu(schur_complement)
+    else
+        schur_complement_factorization = Nothing
+    end
+
     sc_factorization = MPISchurComplement(A_factorization, Ainv_dot_B, C,
                                           schur_complement,
                                           schur_complement_factorization, Ainv_dot_u,
-                                          top_vec_buffer, bottom_vec_buffer)
+                                          top_vec_buffer, bottom_vec_buffer,
+                                          shared_MPI_comm, shared_comm_rank,
+                                          shared_comm_size)
 
     return sc_factorization
 end
@@ -96,9 +116,15 @@ function update_schur_complement!(sc::MPISchurComplement, A_factorization, B, C,
     sc.A_factorization = A_factorization
     ldiv!(sc.Ainv_dot_B, A_factorization, B)
     sc.C = C
-    mul!(sc.schur_complement, C, sc.Ainv_dot_B)
-    @. sc.schur_complement = D - sc.schur_complement
-    sc.schur_complement_factorization = lu!(sc.schur_complement)
+    if sc.shared_comm !== nothing
+        MPI.Barrier(sc.shared_comm)
+    end
+
+    if sc.shared_comm_rank == 0
+        mul!(sc.schur_complement, C, sc.Ainv_dot_B)
+        @. sc.schur_complement = D - sc.schur_complement
+        sc.schur_complement_factorization = lu!(sc.schur_complement)
+    end
 
     return nothing
 end
@@ -116,15 +142,22 @@ function ldiv!(x, y, sc::MPISchurComplement, u, v)
     Ainv_dot_u = sc.Ainv_dot_u
     top_vec_buffer = sc.top_vec_buffer
     bottom_vec_buffer = sc.bottom_vec_buffer
+    shared_comm = sc.shared_comm
+    shared_comm_rank = sc.shared_comm_rank
 
     ldiv!(Ainv_dot_u, A_factorization, u)
+    if shared_comm !== nothing
+        MPI.Barrier(shared_comm)
+    end
 
-    mul!(bottom_vec_buffer, C, Ainv_dot_u)
-    @. bottom_vec_buffer = v - bottom_vec_buffer
-    ldiv!(y, schur_complement_factorization, bottom_vec_buffer)
+    if shared_comm_rank == 0
+        mul!(bottom_vec_buffer, C, Ainv_dot_u)
+        @. bottom_vec_buffer = v - bottom_vec_buffer
+        ldiv!(y, schur_complement_factorization, bottom_vec_buffer)
 
-    mul!(top_vec_buffer, Ainv_dot_B, y)
-    @. x = Ainv_dot_u - top_vec_buffer
+        mul!(top_vec_buffer, Ainv_dot_B, y)
+        @. x = Ainv_dot_u - top_vec_buffer
+    end
 
     return nothing
 end
