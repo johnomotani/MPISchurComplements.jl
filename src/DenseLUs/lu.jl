8cd329e5559543eda28b7a4d728bebfebcbe8e4b
John Omotani <john.omotani@ukaea.uk>
2026-02-17 09:39:36 +0000

    Split up DenseLUs into separate files
    ...and move into a subdirectory. Makes code easier to navigate,
    especially once we have added our own LU factorization code.

---
+++ src/DenseLUs/lu.jl
@@ -0,0 +1,45 @@
+function lu!(A_lu::DenseLU{T}, A::AbstractMatrix{T}) where T
+    factors = A_lu.factors
+    row_permutation = A_lu.row_permutation
+    my_L_tiles = A_lu.my_L_tiles
+    my_L_tile_row_ranges = A_lu.my_L_tile_row_ranges
+    my_L_tile_col_ranges = A_lu.my_L_tile_col_ranges
+    my_U_tiles = A_lu.my_U_tiles
+    my_U_tile_row_ranges = A_lu.my_U_tile_row_ranges
+    my_U_tile_col_ranges = A_lu.my_U_tile_col_ranges
+    distributed_comm = A_lu.distributed_comm
+    synchronize_shared = A_lu.synchronize_shared
+    check_lu = A_lu.check_lu
+
+    if A_lu.is_root
+        # Factorize in serial for now. Could look at implementing a parallel version of
+        # this later. Could maybe borrow algorithms from
+        # https://github.com/JuliaLinearAlgebra/RecursiveFactorization.jl/ ?
+        factorization = lu!(A; check=check_lu)
+
+        factors .= factorization.factors
+        row_permutation .= factorization.p
+    end
+    if A_lu.shared_comm_rank == 0
+        req1 = temp_Ibcast!(factors, distributed_comm; root=0)
+        req2 = temp_Ibcast!(row_permutation, distributed_comm; root=0)
+        MPI.Waitall([req1, req2])
+    end
+    synchronize_shared()
+
+    for (step, (row_range, col_range)) ∈ enumerate(zip(my_L_tile_row_ranges,
+                                                       my_L_tile_col_ranges))
+        if !isempty(row_range)
+            @views my_L_tiles[1:length(row_range),1:length(col_range),step] .= factors[row_range, col_range]
+        end
+    end
+
+    for (step, (row_range, col_range)) ∈ enumerate(zip(my_U_tile_row_ranges,
+                                                       my_U_tile_col_ranges))
+        if !isempty(row_range)
+            @views my_U_tiles[1:length(row_range),1:length(col_range),step] .= factors[row_range, col_range]
+        end
+    end
+
+    return A_lu
+end
