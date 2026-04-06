9f32ce19f3dd62e0660297f1f571754bd4942ae3
John Omotani <john.omotani@ukaea.uk>
2026-04-06 11:58:02 +0100

    Use at most 2 threads in fortran benchmark
    It seems like OpenBLAS does not use threads efficiently for the problem
    sizes we are trying. Test 2 threads just to see if it gives any
    improvement, but don't test more to save time.

--- performance-benchmark/run-fortran-benchmark-32-cores.sh
+++ performance-benchmark/run-fortran-benchmark-32-cores.sh
@@ -2,38 +2,39 @@
 
 # For each number of MPI ranks, use all possible numbers of BLAS threads while
 # staying within 32 physical cores.
-for NT in {1..32}; do
-  OPENBLAS_NUM_THREADS=$NT mpirun -np 1 ./fortran-benchmark matrices-and-rhs.h5
-done
-for NT in {1..16}; do
-  OPENBLAS_NUM_THREADS=$NT mpirun -np 2 ./fortran-benchmark matrices-and-rhs.h5
-done
-for NT in {1..10}; do
-  OPENBLAS_NUM_THREADS=$NT mpirun -np 3 ./fortran-benchmark matrices-and-rhs.h5
-done
-for NT in {1..8}; do
-  OPENBLAS_NUM_THREADS=$NT mpirun -np 4 ./fortran-benchmark matrices-and-rhs.h5
-done
-for NT in {1..6}; do
-  OPENBLAS_NUM_THREADS=$NT mpirun -np 5 ./fortran-benchmark matrices-and-rhs.h5
-done
-for NT in {1..5}; do
-  OPENBLAS_NUM_THREADS=$NT mpirun -np 6 ./fortran-benchmark matrices-and-rhs.h5
-done
-for NT in {1..4}; do
-  OPENBLAS_NUM_THREADS=$NT mpirun -np 7 ./fortran-benchmark matrices-and-rhs.h5
-done
-for NT in {1..4}; do
-  OPENBLAS_NUM_THREADS=$NT mpirun -np 8 ./fortran-benchmark matrices-and-rhs.h5
-done
-for NT in {1..3}; do
-  OPENBLAS_NUM_THREADS=$NT mpirun -np 9 ./fortran-benchmark matrices-and-rhs.h5
-done
-for NT in {1..3}; do
-  OPENBLAS_NUM_THREADS=$NT mpirun -np 10 ./fortran-benchmark matrices-and-rhs.h5
-done
+#for NT in {1..32}; do
+#  OPENBLAS_NUM_THREADS=$NT mpirun -np 1 ./fortran-benchmark matrices-and-rhs.h5
+#done
+#for NT in {1..16}; do
+#  OPENBLAS_NUM_THREADS=$NT mpirun -np 2 ./fortran-benchmark matrices-and-rhs.h5
+#done
+#for NT in {1..10}; do
+#  OPENBLAS_NUM_THREADS=$NT mpirun -np 3 ./fortran-benchmark matrices-and-rhs.h5
+#done
+#for NT in {1..8}; do
+#  OPENBLAS_NUM_THREADS=$NT mpirun -np 4 ./fortran-benchmark matrices-and-rhs.h5
+#done
+#for NT in {1..6}; do
+#  OPENBLAS_NUM_THREADS=$NT mpirun -np 5 ./fortran-benchmark matrices-and-rhs.h5
+#done
+#for NT in {1..5}; do
+#  OPENBLAS_NUM_THREADS=$NT mpirun -np 6 ./fortran-benchmark matrices-and-rhs.h5
+#done
+#for NT in {1..4}; do
+#  OPENBLAS_NUM_THREADS=$NT mpirun -np 7 ./fortran-benchmark matrices-and-rhs.h5
+#done
+#for NT in {1..4}; do
+#  OPENBLAS_NUM_THREADS=$NT mpirun -np 8 ./fortran-benchmark matrices-and-rhs.h5
+#done
+#for NT in {1..3}; do
+#  OPENBLAS_NUM_THREADS=$NT mpirun -np 9 ./fortran-benchmark matrices-and-rhs.h5
+#done
+#for NT in {1..3}; do
+#  OPENBLAS_NUM_THREADS=$NT mpirun -np 10 ./fortran-benchmark matrices-and-rhs.h5
+#done
 for NT in {1..2}; do
-  for NP in {11..16}; do
+  #for NP in {11..16}; do
+  for NP in {1..16}; do
     OPENBLAS_NUM_THREADS=$NT mpirun -np $NP ./fortran-benchmark matrices-and-rhs.h5
   done
 done
