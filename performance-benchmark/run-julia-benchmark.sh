63e14dbd47624f8076a6d309efbe092f987cb362
John Omotani <john.omotani@ukaea.uk>
2026-04-07 11:02:44 +0100

    Save terminal output of benchmarks to log files

--- performance-benchmark/run-julia-benchmark.sh
+++ performance-benchmark/run-julia-benchmark.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for NP in $(seq 1 $1); do
-  mpirun -np $NP julia --project -O3 -J julia-benchmark.so julia-benchmark.jl matrices-and-rhs.h5
+  mpirun -np $NP julia --project -O3 -J julia-benchmark.so julia-benchmark.jl matrices-and-rhs.h5 2>&1 | tee -a log-julia-benchmark.log
 done
